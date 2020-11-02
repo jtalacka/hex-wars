b55cfada771bdf614871393b449471aa57b5f839
lukasbaltramaitis <43213136+lukasbaltramaitis@users.noreply.github.com>
2020-11-03 00:26:14 +0200

    Player entity added
    Player entity added and Player reference added into Army entity.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class Player : ScriptableObject
+{
+    public int id;
+    public List<Army> armies;
+    public int money_amount;
+    public int province_nr;
+}
